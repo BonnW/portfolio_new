970922c413bceb77e0856a133c6280df301b27f5
BonnW <pwonghansa@gmail.com>
2021-09-01 01:29:32 -0500

    added more files for sample sidebar. cleaned up app.js and set up react-router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import * as faIcons from 'react-icons/fa';
 
 function Navbar() {
   return (
       <>
         <div className='navBar'>
-          
+          <Link to="#" className='menu-bars'>
+            <faIcons.FaBars />
+          </Link>
         </div>
       </>
   )
 }
 
+export default Navbar;
