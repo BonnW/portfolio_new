758d85b240ca65a6e41aff976a505f2856bb61f7
BonnW <pwonghansa@gmail.com>
2021-09-01 02:13:20 -0500

    added css

---
+++ src/components/SidebarData.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Home0',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+
+  {
+    title: 'Home1',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+
+  {
+    title: 'Home2',
+    path: '/',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+
+  {
+    title: 'Home3',
+    path: '/',
+    icon: <FaIcons.FaCartPlus />,
+    cName: 'nav-text'
+  },
+]
